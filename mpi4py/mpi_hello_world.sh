ff3a5e3933dd017b94ce995db8d6939dbba316b7
Marko Kvakic <mkvakic@srce.hr>
2024-01-15 15:23:43 +0100

    wrf install

--- mpi4py/mpi_hello_world.sh
+++ mpi4py/mpi_hello_world.sh
@@ -13,17 +13,29 @@ export APPTAINERENV_LD_LIBRARY_PATH="${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}:
 # export APPTAINERENV_LD_LIBRARY_PATH="${CRAY_LD_LIBRARY_PATH}:\$LD_LIBRARY_PATH:/opt/cray/pe/pals/1.2.3/lib"
 
 # ldd
+echo '---------- ldd ----------'
 singularity exec \
   --bind /opt \
   --bind /run \
-  mpi4py-install \
+  mpi4py-install.sif \
     ldd /myapp/mpi_hello_world
 
 # mpi_hello_world
+echo '---------- mpi_hello_world ----------'
 mpiexec \
   singularity exec \
     --bind /opt \
     --bind /run \
     --bind /usr/lib64 \
-    mpi4py-install \
+    mpi4py-install.sif \
       /myapp/mpi_hello_world
+
+# mpi_hello_world.py
+echo '---------- mpi_hello_world.py ----------'
+mpiexec \
+  singularity exec \
+    --bind /opt \
+    --bind /run \
+    --bind /usr/lib64 \
+    mpi4py-install.sif \
+      python3 /myapp/mpi_hello_world.py
